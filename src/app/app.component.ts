87f11966ccd1c7bdb26734c74fa66eda0e7c8492
djerdjeramazigh <djerdjeramazigh@gmail.com>
2018-05-14 22:47:15 +0200

    compare two json objects with number entry in string format in the first object and number format in the second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+export class RowData{
+  constructor(public id: Number, public name:String){
+
+  }
+}
+
 @Component({
-  selector: 'app-root',
+ selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+  obj={"id":"12", "name":"anna"}
+  obj2={"id":12, "name":"anna"}
+  row:RowData
+  row_: RowData
+ onClick(){
+  debugger
+  this.row=  new RowData(Number(this.obj.id),this.obj.name)
+  this.row_= new RowData(this.obj2.id,this.obj2.name)
+ }
+ compare(){
+   return JSON.stringify(this.row)==JSON.stringify(this.row_)
+ }
+debugger
 }
